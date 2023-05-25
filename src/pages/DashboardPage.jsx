c626e1a562ab74dc76f983a219eef3f493bd4e7d
Cris-casas-react <cristina.casas2@gmail.com>
2023-05-25 21:14:49 +0200

    muestra-nombre-juegos-consola

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,20 +2,23 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const DashboardPage = () => {
-  
+
   useEffect(() => {
-    axios.get('https://api.boardgameatlas.com/api/search?client_id=AUi6AFUDNI')
+    axios.get('https://api.boardgameatlas.com/api/search?limit=100&client_id=AUi6AFUDNI')
       .then(res => {
-        console.log(res.data.games);
+        const games = res.data.games.map(game => game.name);
+        console.log(games);
       })
+      .catch(error => {
+        console.error('Error fetching games:', error);
+      });
   }, []);
 
+  return (
+    <>
+      <div>hola</div>
+    </>
+  );
+};
 
-    return (
-      <>
-        <div>hola</div>
-      </>
-    )
-  }
-  
-  export default DashboardPage;
+export default DashboardPage;
