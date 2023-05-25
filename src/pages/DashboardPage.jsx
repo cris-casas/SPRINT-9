9abcde5de8222a1dddfaef86d006f938af13a65f
Cris-casas-react <cristina.casas2@gmail.com>
2023-05-25 20:09:40 +0200

    llamada-api-ok

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const DashboardPage = () => {
+  
+  useEffect(() => {
+    axios.get('https://api.boardgameatlas.com/api/search?client_id=AUi6AFUDNI')
+      .then(res => {
+        console.log(res.data.games);
+      })
+  }, []);
+
+
     return (
       <>
         <div>hola</div>
